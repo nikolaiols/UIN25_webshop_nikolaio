87270789844762c8c10008331dbc44008f69dc23
nikolaiols <nikolaio@hiof.no>
2025-01-16 08:59:52 +0100

    forelesning 3 før pause

--- Legodudes/shopscript.js
+++ Legodudes/shopscript.js
@@ -12,8 +12,21 @@ products.map((product, index) => productHTML +=
                 <a href="#KATEGORISIDE">Ninjago</a>
                 <h3>${product.title}</h3>
                 <p>${product.price},-</p>
-                <button>Legg i handlekurv</button>
+                <button onclick="addProductToCart(${product.prodid})">Legg i handlekurv</button>
             </article>`)
 
 //finn #productlist og fyll med variabelen productHTML
 document.getElementById("productlist").innerHTML = productHTML
+
+//lage toggle funktionalitet for handlevogn
+document.getElementById("shoppingcart").addEventListener("click", function(){
+    document.getElementById("cart").classList.toggle("visible") //ferdigbygd js if else test.
+})
+
+//lage addProductToCart
+function addProductToCart(prodid){
+    console.log("du vil legge til productid" + prodid)
+
+    cart.push({cartprodid: prodid, quantity: 1})
+    console.log(cart)
+}
