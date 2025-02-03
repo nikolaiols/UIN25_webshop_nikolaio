f88cf718a5ed65e4f669d3c27ff0fbff86f1635f
nikolaiols <nikolaio@hiof.no>
2025-02-03 14:00:18 +0100

    reactprosjekt

---
+++ TODO/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import "../styles/nav.scss";
+
+export default function Nav({username}){
+    
+    
+    return (
+        <header>
+            <nav>
+                <a href="#">ToDo´s</a>
+                <a href="#">{username}</a>
+            </nav>
+        </header>
+    );
+}
