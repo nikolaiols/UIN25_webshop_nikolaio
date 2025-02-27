5d648ad81ea28f0c701fad2c3876297dfb75b28f
nikolaiols <nikolaio@hiof.no>
2025-02-27 09:57:49 +0100

    forelesning 27.02

--- Zelda_api/src/components/CategoryCard.jsx
+++ Zelda_api/src/components/CategoryCard.jsx
@@ -1,9 +1,12 @@
-import "../assets/styles/GameCard.scss"
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import "../assets/styles/gameCard.scss"
+import { Link, useParams } from "react-router-dom";
 
 export default function CategoryCard({item}){
+
+   
     return( 
-    <article className="GameCard">
+    <article className="gameCard">
         <h3>{item.name}</h3>
         <Link to={item.id}>Les mer om {item.name}</Link>
     </article>)
--- Zelda_api/src/components/ContentPage.jsx
+++ Zelda_api/src/components/ContentPage.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function ContentPage(){
-    const {content} = useParams();
-    return <h1>{content}</h1>
+    const {slug, content} = useParams();
+    const [pageContent, setpageContent] = useState()
+
+  
+
+    const getContent = async() => {
+        fetch(`https://zelda.fanapis.com/api/${slug}/${content}`)
+        .then(response => response.json())
+        .then((data) => setpageContent(data.data))
+        .catch((error) => console.error("skjedde noe rart med henting av innhold", error))
+    };
+    useEffect(()=>{
+        getContent();
+        console.log(pageContent)
+    }, [content]);
+
+    return (
+    <>
+    <h1>{pageContent?.name}</h1>
+    <section>
+        {pageContent?.inhabitants ? 
+       <>
+       <h2>inhabitants</h2>
+        <ul>
+            {/*disse må fetches*/}
+        <li>{pageContent?.inhabitants}</li>
+        </ul>
+        </>
+        : null} 
+        
+    </section>
+    </>
+)
+
 }
