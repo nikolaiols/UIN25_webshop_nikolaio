e9b6c72d5daf19f8c39676618ee4f9e3017b21f5
nikolaiols <nikolaio@hiof.no>
2025-03-06 10:01:09 +0100

    css

--- LocalStorage/src/components/Layout.jsx
+++ LocalStorage/src/components/Layout.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import "../styles/layout.scss"
 
 export default function Layout ({children}){
     return(
--- LocalStorage/src/components/Signup.jsx
+++ LocalStorage/src/components/Signup.jsx
@@ -1,24 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function Signup (){
+    const [User, setUser] = useState([]);
+    const navigate = useNavigate()
+
+    const handleChange = (e)=>{
+
+        const inputValue = e.target.value;
+        const inputName = e.target.name;
+
+        setUser((prev) => ({ ...prev, [inputName]: inputValue}));
+
+        console.log(User);
+    }
+
+    const handleClick = (e)=>{
+        e.preventDefault()
+        localStorage.setItem("user", JSON.stringify(User))
+        navigate("/login");
+    }
+
     return(
         <section>
         <h1>Registrer bruker</h1>
         <form>
             <label>
                 E-post
-                <input type="email" placeholder="nikolaio@gmail.com" />
+                <input name="email" onChange={handleChange} type="email" placeholder="nikolaio@..." />
             </label>
             <label>
                 fullt navn
-                <input type="text" placeholder="Nikolai Kruge Olsen" />
+                <input name="name" onChange={handleChange} type="text" placeholder="Nikolai Kruge" />
             </label>
             <label>
                 Brukernavn
-                <input type="text" placeholder="OlaNordman..."/>
+                <input name="username" onChange={handleChange} type="text" placeholder="OlaNordman..."/>
             </label>
-            <label>Passord
-                <input type="password" placeholder="*********" />
+            <label>
+                Passord
+                <input name="password" onChange={handleChange} type="password" placeholder="*********" />
             </label>
-            <button>Registrer</button>
+            <button onClick={handleClick}>Registrer</button>
         </form>
         </section>
     );
