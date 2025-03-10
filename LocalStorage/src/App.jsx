d6d925c6e50ff8e79ddc33275f678e312cef0875
nikolaiols <nikolaio@hiof.no>
2025-03-10 13:07:52 +0100

    login page

--- LocalStorage/src/App.jsx
+++ LocalStorage/src/App.jsx
@@ -8,14 +8,18 @@ import Signup from './components/Signup'
 
 function App() {
  const [SignedIn, setSignedIn] = useState(false);
+ 
+ const [storageUser, setStorageUser] = useState(localStorage.getItem("user"));
+
+ console.log("kommer fra storage:",storageUser);
 
   return (
     <>
     <Layout>
       {/**alt som legges her blir rendret imellom header og footer, eller bassert på hvor du legger children i layout */}
       <Routes>
-        <Route path='/' element={SignedIn ?<Welcome/> : <Login/>}/>
-        <Route path='login' element={<Login/>}/>
+        <Route path='/' element={SignedIn ?<Welcome/> : <Login storageUser={storageUser} setSignedIn={setSignedIn}/>}/>
+        <Route path='login' element={<Login storageUser={storageUser} setSignedIn={setSignedIn}/>}/>
         <Route path='signup' element={<Signup/>}/>
       </Routes>
 
