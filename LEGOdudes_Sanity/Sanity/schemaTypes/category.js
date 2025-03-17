dee929bf33c1355e80249b2154284cc2c068f644
nikolaiols <nikolaio@hiof.no>
2025-03-17 13:59:00 +0100

    Sanity downlload

---
+++ LEGOdudes_Sanity/Sanity/schemaTypes/category.js
@@ -0,0 +1,12 @@
+export const category = {
+    type: "document", 
+    name: "category",
+    title: "Kategori",
+    fields: [
+        {
+            name: "categoryname",
+            title: "Kategorinavn",
+            type: "string"
+        }
+    ]
+}
