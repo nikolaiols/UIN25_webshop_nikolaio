9908ab878cce3242c90acca69a3537d617171887
nikolaiols <nikolaio@hiof.no>
2025-04-02 22:04:36 +0200

    fjernet unødvendig

--- abk_test/src/App.jsx
+++ abk_test/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
