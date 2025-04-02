60fb25fd83e1b8ad2efa9f37084d702cab80e278
nikolaiols <nikolaio@hiof.no>
2025-04-02 22:46:48 +0200

    ..

--- abk_test/src/components/Home.jsx
+++ abk_test/src/components/Home.jsx
@@ -3,7 +3,8 @@ export default function Home(){
         <h2>HJEM</h2>
         /* -her skal det "medlemskort og en felles arbeidslogg"
             - dette skal skrives ut via sanity
+            - kortet skal inneholde bilde, navn og epost 
         */
-       
+
     );
 }
